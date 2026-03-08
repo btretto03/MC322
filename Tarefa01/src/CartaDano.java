135b32d419ff94aac41718fd12f0b4b3affb30b0
Bruno A. Tretto <bruno@Bruno.localdomain>
2026-03-07 23:13:19 -0300

    structure of Carta's classes

--- Tarefa01/src/CartaDano.java
+++ Tarefa01/src/CartaDano.java
@@ -1,3 +1,32 @@
 public class CartaDano {
+    private String nome = "Golpe(pensar em um nome melhor)";
+    private int custo = 2; //podemos mudar esses valores, só to colocando e depois com ele pronto testamos como ficar melhor da pra ver pra ser aleatorio tambem(achei legal como fiz no ataque do inimigo)
+
+    public CartaDano(String nome, int custo) {
+        this.nome = nome;
+        this.custo = custo;
+    }
+    public void usar() {
+
+    }
+
+    //getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCusto() {
+        return custo;
+    }
+
+    public void setCusto(int custo) {
+        this.custo = custo;
+    }
     
+
+
 }
--- Tarefa01/src/CartaEscudo.java
+++ Tarefa01/src/CartaEscudo.java
@@ -1,3 +1,26 @@
 public class CartaEscudo {
-    
+    private String nome = "Escudo(pensar em um nome melhor)";
+    private int custo = 2; // dnv podemos mudar
+
+    public CartaEscudo(String nome, int custo) {
+        this.nome = nome;
+        this.custo = custo;
+    }
+    public void usar() {
+
+    }
+
+    //getters e setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCusto() {
+        return custo;
+    }
+    public void setCusto(int custo) {
+        this.custo = custo;
+    }
 }
