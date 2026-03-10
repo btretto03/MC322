da7ce9910c411a72c78c4232cfe670b04e39681e
João Felipe Denadai Madeira <258477@dac.unicamp.br>
2026-03-10 15:06:08 -0300

    fix main (Reset cards after each round)

--- Tarefa01/src/App.java
+++ Tarefa01/src/App.java
@@ -7,24 +7,36 @@ public class App {
         Scanner inputs = new Scanner(System.in); //leitura de dados do usuário
 
         String[] nomeAtaques = {"Cruzado de direita", "Gancho de direita", "Gancho de esquerda", "Uppercut", "Cruzado de esquerda"}; //pensei nisso pra quando formos printar que ele usou um ataque fazer um loop nessa lista, só pra ficar uns nomes diferentes... mesma coisa na de baixo
-        List<CartaDano> Ataques = new ArrayList<>();
-        for(int i = 0; i < nomeAtaques.length; i++){
-            int custo = (int) (Math.random() * 3) + 1;
-            Ataques.add(new CartaDano(nomeAtaques[i], custo));
-        }
+        // List<CartaDano> Ataques = new ArrayList<>();
+        // for(int i = 0; i < nomeAtaques.length; i++){
+        //     int custo = (int) (Math.random() * 3) + 1;
+        //     Ataques.add(new CartaDano(nomeAtaques[i], custo));
+        // }
         
         String[] nomeEscudos = {"Esquivo para direita", "Bloqueio", "Esquivo para esquerda", "Esquivo para trás"};
-        List<CartaEscudo> Escudos = new ArrayList<>();
-        for(int i = 0; i < nomeEscudos.length; i++){
-            int custo = (int) (Math.random() * 3) + 1;
-            Escudos.add(new CartaEscudo(nomeEscudos[i], custo));
-        }
+        // List<CartaEscudo> Escudos = new ArrayList<>();
+        // for(int i = 0; i < nomeEscudos.length; i++){
+        //     int custo = (int) (Math.random() * 3) + 1;
+        //     Escudos.add(new CartaEscudo(nomeEscudos[i], custo));
+        // }
 
         Heroi alex = new Heroi();
         Inimigo jon = new Inimigo();
 
         while(true) {
             alex.setEnergia(3); //energia do poatan é resetada a cada turno
+
+            List<CartaDano> Ataques = new ArrayList<>();
+            for(int i = 0; i < nomeAtaques.length; i++){
+                int custo = (int) (Math.random() * 3) + 1;
+                Ataques.add(new CartaDano(nomeAtaques[i], custo));
+            }
+
+            List<CartaEscudo> Escudos = new ArrayList<>();
+            for(int i = 0; i < nomeEscudos.length; i++){
+                int custo = (int) (Math.random() * 3) + 1;
+                Escudos.add(new CartaEscudo(nomeEscudos[i], custo));
+            }
 
             if (alex.estaVivo() == true && jon.estaVivo() == true) { //Os dois vivos
                 String vidaAlex = String.format("Alex está com %d de vida", alex.getVida());
