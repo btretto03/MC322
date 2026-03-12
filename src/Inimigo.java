1b33250ebbe3e0b1f11c04fa04467a372d50153e
João Felipe Denadai Madeira <258477@dac.unicamp.br>
2026-03-12 13:22:56 -0300

    Change on Inimigo (Adapts the class to extend Entidade)

--- src/Inimigo.java
+++ src/Inimigo.java
@@ -1,68 +1,13 @@
-public class Inimigo {
-    private String nome; //
-    private int vida;
-    private int escudo;
+public class Inimigo extends Entidade {
     private int forca = 0;
     
     public Inimigo(String nome, int vida, int escudo) { //construtor
-        this.nome = nome;
-        this.vida = vida;
-        this.escudo = escudo;
+        super(nome, vida, escudo);
     }
     
-
     //Métodos
-    public void receberDano (int dano) {
-        int escudoAtual = this.getEscudo();
-        if(escudoAtual > 0) {
-            int danoEfetivo = dano - escudoAtual;
-            if (dano < escudoAtual){
-                this.escudo -= dano;
-            } else {
-                this.vida -= danoEfetivo;
-                this.escudo = 0;
-            }
-        } else {
-            this.vida -= dano;
-        }
-    }
-
     public void atacar(Heroi atacado) {
         this.forca = (int) (Math.random() * 5) + 6; //numero aleatorio para o ataque no range de 6 até 10
         atacado.receberDano(this.forca);
-    }
-
-    public boolean estaVivo() {
-        if(this.getVida() > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    //getters e setters
-    public String getNome() {
-        return this.nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getVida() {
-        return this.vida;
-    }
-
-    public void setVida(int vida) {
-        this.vida = vida;
-    }
-
-    public int getEscudo() {
-        return this.escudo;
-    }
-
-    public void setEscudo(int escudo) {
-        this.escudo = escudo;
-    }
-    
+    }    
 }
