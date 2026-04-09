32a1160ec4d88ab8fc9526a703a11cbb68011163
João Felipe Denadai Madeira <258477@dac.unicamp.br>
2026-04-09 15:30:59 -0300

    Add on Nocaute (Add new atributte so Nocaute can see Inimigos array)

--- app/src/main/java/Efeitos/Nocaute.java
+++ app/src/main/java/Efeitos/Nocaute.java
@@ -1,17 +1,30 @@
 package Efeitos;
 
 import Prints.PrintsMain;
+import java.util.ArrayList;
+
+import Entidades.Inimigo;
 
 public class Nocaute extends Efeitos{
     private boolean nocauteado = false;
+    private ArrayList <Inimigo> inimigos;
 
-    public Nocaute(String nome, Entidades.Entidade dono){
+    public Nocaute(String nome, Entidades.Entidade dono, ArrayList <Inimigo> inimigos){
         super(nome, 0, dono);
+        this.inimigos = inimigos;
     }
 
     @Override
     public void aplicarEfeito(){
-        int tentativa = (int) Math.random() * 11;
+        int probabilidade;
+
+        if (inimigos.size() == 2){
+            probabilidade = 6;
+        } else {
+            probabilidade = 11;
+        }
+
+        int tentativa = (int) Math.random() * probabilidade;
 
         if (tentativa == 1){
             dono.setVida(0);
@@ -24,6 +37,10 @@ public class Nocaute extends Efeitos{
 
     public void setNocauteado (boolean valor){
         this.nocauteado = valor;
+    }
+
+    public void setInimigos (ArrayList<Inimigo> inimigos){
+        this.inimigos = inimigos;
     }
 
     public boolean getNocauteado(){
