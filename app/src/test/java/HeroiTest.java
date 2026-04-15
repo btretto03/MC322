eb2f1ab3e0f054079cfd97c6f4c49b6e0f387bf4
João Felipe Denadai Madeira <258477@dac.unicamp.br>
2026-04-15 11:19:24 -0300

    Add: Add tests for Heroi

---
+++ app/src/test/java/HeroiTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import Entidades.Heroi;
+import Cartas.Carta;
+import Cartas.CartaDano;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HeroiTest {
+    @Test
+    void escolherHeroiTest (){
+        for (int i = 1; i < 4; i++){
+            String inputSimulado = "" + i + "\n";
+            ByteArrayInputStream in = new ByteArrayInputStream(inputSimulado.getBytes());
+
+            Scanner inputs = new Scanner(in);
+
+            if (i == 1){
+                assertEquals("Alex Poatan", Heroi.escolherHeroi(inputs));
+            } else if (i == 2){
+                assertEquals("Anderson Silva", Heroi.escolherHeroi(inputs));
+            } else if (i == 3){
+                assertEquals("Fabrício Werdum", Heroi.escolherHeroi(inputs));
+            } 
+        }
+
+    }
+
+    @Test
+    void verificaMao_valida_Test (){
+        Heroi heroi = new Heroi("Teste", 1, 1, 1);
+        heroi.setEnergia(10);
+        ArrayList<Carta> mao = new ArrayList<>();
+
+        Carta carta = new CartaDano("carta teste", 1);
+        mao.add(carta);
+
+        assertTrue(heroi.verificaMao(mao));
+    }
+
+    @Test
+    void verificaMao_invalida_Test (){
+        Heroi heroi = new Heroi("Teste", 1, 1, 1);
+        heroi.setEnergia(0);
+        ArrayList<Carta> mao = new ArrayList<>();
+
+        Carta carta = new CartaDano("carta teste", 1);
+        mao.add(carta);
+
+        assertFalse(heroi.verificaMao(mao));
+    }
+}
